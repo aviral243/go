449b5a0c88775c2f637fc72954376669bb8c5f85
aviral243 <aviralgangwar24@gmail.com>
2020-06-07 15:33:54 +0530

    interfaces advanced

---
+++ interfaces/http/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	// io.Copy(os.Stdout, resp.Body)
+
+	// Custom implementation of Writer interface
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
